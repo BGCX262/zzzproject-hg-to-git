85013708da991083dd799c9ecc52aac500d02f71
tregubov maxim <tregubov.maxim@gmail.com>
2011-09-20 01:50:22 +0400

    Fix a bug in encoding

--- Table creation and population scripts/Create_or_replace_views.sql
+++ Table creation and population scripts/Create_or_replace_views.sql
@@ -1,76 +1,108 @@
-﻿
 create or replace view v_dates as 
-with
-all_dates as 
-(  
-  select to_date('31.12.2006','dd.mm.yyyy')+rownum dt 
-  from dual 
-  connect by level <= (sysdate+365 - to_date('01.01.2007','dd.mm.yyyy'))
-)
+AS
+WITH all_dates AS
+  (SELECT to_date('31.12.2006','dd.mm.yyyy')+rownum dt
+  FROM dual
+    CONNECT BY level <= (sysdate+365 - to_date('01.01.2007','dd.mm.yyyy'))
+  )
 --days
-select to_char(dt,'yyyymmdd') as dt_id, 
-to_char(dt,'yyyymmdd') as dt_id_fake, 
-to_char(dt,'dd.mm.yyyy') as dt, 
-to_char(dt,'yyyymm') as dt_parent
-from all_dates
-
-union
-
+SELECT TO_CHAR(dt,'yyyymmdd') AS dt_id,
+  TO_CHAR(dt,'yyyymmdd')      AS dt_id_fake,
+  TO_CHAR(dt,'dd.mm.yyyy')    AS dt,
+  TO_CHAR(dt,'yyyymm')        AS dt_parent
+FROM all_dates
+UNION
 --months
-select distinct dt_id, dt_id_fake,dt,dt_parent
-from 
-(
-select to_char(dt,'yyyymm') as dt_id, 
-to_char(add_months(last_day(dt)+1,-1),'yyyymmdd') as dt_id_fake, 
-to_char(dt,'Month') as dt, 
-to_char(dt,'yyyy')||'Q'||to_char(dt,'q') as dt_parent
-from all_dates
-)
-union
-
+SELECT DISTINCT dt_id,
+  dt_id_fake,
+  dt,
+  dt_parent
+FROM
+  (SELECT TO_CHAR(dt,'yyyymm')                        AS dt_id,
+    TO_CHAR(add_months(last_day(dt)+1,-1),'yyyymmdd') AS dt_id_fake,
+    TO_CHAR(dt,'Month')                               AS dt,
+    TO_CHAR(dt,'yyyy')
+    ||'Q'
+    ||TO_CHAR(dt,'q') AS dt_parent
+  FROM all_dates
+  )
+UNION
 --quarters
-select distinct dt_id,dt_id_fake,dt, dt_parent
-from 
-(
-select to_char(dt,'yyyy')||'Q'||to_char(dt,'q') as dt_id, 
-case when to_number(to_char(dt,'q')) = 1 then to_char(dt,'yyyy') || '0101'
-     when to_number(to_char(dt,'q')) = 2 then to_char(dt,'yyyy') || '0401'
-     when to_number(to_char(dt,'q')) = 3 then to_char(dt,'yyyy') || '0701'
-     when to_number(to_char(dt,'q')) = 4 then to_char(dt,'yyyy') || '1001'
-     end dt_id_fake,
-'Q'||to_char(dt,'Q') as dt, 
-case when to_number(to_char(dt,'mm'))>6 then 'H2'|| to_char(dt,'YYYY') else 'H1'|| to_char(dt,'YYYY') end  as dt_parent
---to_char(dt,'YYYY') as dt_parent
-from all_dates
-)
-
-union
-
+SELECT DISTINCT dt_id,
+  dt_id_fake,
+  dt,
+  dt_parent
+FROM
+  (SELECT TO_CHAR(dt,'yyyy')
+    ||'Q'
+    ||TO_CHAR(dt,'q') AS dt_id,
+    CASE
+      WHEN to_number(TO_CHAR(dt,'q')) = 1
+      THEN TO_CHAR(dt,'yyyy')
+        || '0101'
+      WHEN to_number(TO_CHAR(dt,'q')) = 2
+      THEN TO_CHAR(dt,'yyyy')
+        || '0401'
+      WHEN to_number(TO_CHAR(dt,'q')) = 3
+      THEN TO_CHAR(dt,'yyyy')
+        || '0701'
+      WHEN to_number(TO_CHAR(dt,'q')) = 4
+      THEN TO_CHAR(dt,'yyyy')
+        || '1001'
+    END dt_id_fake,
+    'Q'
+    ||TO_CHAR(dt,'Q') AS dt,
+    CASE
+      WHEN to_number(TO_CHAR(dt,'mm'))>6
+      THEN 'H2'
+        || TO_CHAR(dt,'YYYY')
+      ELSE 'H1'
+        || TO_CHAR(dt,'YYYY')
+    END AS dt_parent
+    --to_char(dt,'YYYY') as dt_parent
+  FROM all_dates
+  )
+UNION
 --half years
-
-select distinct dt_id,dt_id_fake,dt,dt_parent
-from 
-(
-select
-case when to_number(to_char(dt,'mm'))>6 then 'H2'||to_char(dt,'YYYY') else 'H1'||to_char(dt,'YYYY') end  as dt_id, 
-case when to_number(to_char(dt,'mm'))>6 then to_char(dt,'YYYY') || '0701' else to_char(dt,'YYYY') || '0101' end as dt_id_fake,
-case when to_number(to_char(dt,'mm'))>6 then 'H2' else 'H1' end as dt, 
-to_char(dt,'YYYY') as dt_parent
-from all_dates
-)
-
-union
-
+SELECT DISTINCT dt_id,
+  dt_id_fake,
+  dt,
+  dt_parent
+FROM
+  (SELECT
+    CASE
+      WHEN to_number(TO_CHAR(dt,'mm'))>6
+      THEN 'H2'
+        ||TO_CHAR(dt,'YYYY')
+      ELSE 'H1'
+        ||TO_CHAR(dt,'YYYY')
+    END AS dt_id,
+    CASE
+      WHEN to_number(TO_CHAR(dt,'mm'))>6
+      THEN TO_CHAR(dt,'YYYY')
+        || '0701'
+      ELSE TO_CHAR(dt,'YYYY')
+        || '0101'
+    END AS dt_id_fake,
+    CASE
+      WHEN to_number(TO_CHAR(dt,'mm'))>6
+      THEN 'H2'
+      ELSE 'H1'
+    END                AS dt,
+    TO_CHAR(dt,'YYYY') AS dt_parent
+  FROM all_dates
+  )
+UNION
 --years
-select distinct dt_id,dt_id_fake,dt, dt_parent
-from 
-(
-select to_char(dt,'yyyy') as dt_id,
-to_char(dt,'yyyy') || '0101' as dt_id_fake,
-to_char(dt,'YYYY') as dt, 
-null as dt_parent
-from all_dates
-)
-;
-
-
+SELECT DISTINCT dt_id,
+  dt_id_fake,
+  dt,
+  dt_parent
+FROM
+  (SELECT TO_CHAR(dt,'yyyy') AS dt_id,
+    TO_CHAR(dt,'yyyy')
+    || '0101'          AS dt_id_fake,
+    TO_CHAR(dt,'YYYY') AS dt,
+    NULL               AS dt_parent
+  FROM all_dates
+  );
